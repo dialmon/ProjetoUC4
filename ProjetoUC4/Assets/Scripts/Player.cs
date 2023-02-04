bd45059bbfd865f324d6578a837bed88a4999587
Tatiana Oshiro Kobayashi <tatianakobayashi@hotmail.com>
2023-02-03 21:51:46 -0400

    renomeando script

--- ProjetoUC4/Assets/Scripts/Player.cs
+++ ProjetoUC4/Assets/Scripts/Player.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Script2 : MonoBehaviour
+public class Player : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -14,5 +14,10 @@ public class Script2 : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void Movement()
+    {
+
     }
 }
