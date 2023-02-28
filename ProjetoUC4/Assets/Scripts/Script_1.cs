3bfe558f49c345e7092f5731fc56fa7a8100e75c
akame-robot <wolfinfiresnow@gmail.com>
2023-02-27 21:35:29 -0400

    commit ammo

--- ProjetoUC4/Assets/Scripts/Script_1.cs
+++ ProjetoUC4/Assets/Scripts/Script_1.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.XR;
@@ -26,6 +27,10 @@ public class Script_1 : MonoBehaviour
 
     private Camera mainCam;
     private Vector3 mousePos;
+
+    //textmesh
+
+    public TextMeshProUGUI text;
 
 
     void Start()
@@ -60,6 +65,8 @@ public class Script_1 : MonoBehaviour
 
         myInput();
         Reload();
+
+        text.SetText(bulletsLeft + "/" + magazineSize);
 
     }
     public void myInput()
