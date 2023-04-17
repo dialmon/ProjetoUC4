eb6ed5ff5f31d8230efef7a0e1bc64ee5ff24273
akame-robot <wolfinfiresnow@gmail.com>
2023-04-17 19:03:18 -0400

    commit leveldesigm

--- ProjetoUC4/Assets/Scripts/PlayerLife.cs
+++ ProjetoUC4/Assets/Scripts/PlayerLife.cs
@@ -7,6 +7,8 @@ public class PlayerLife : MonoBehaviour
 {
     public int playerLife, playerMaxLife = 100;
 
+    //aleatorio
+
 
     // Start is called before the first frame update
     void Start()
